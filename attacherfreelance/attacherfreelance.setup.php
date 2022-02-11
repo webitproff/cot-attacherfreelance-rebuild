2c8a2b93d3b4af0837d6151ea1d65ac9ae0fee59
webitproff <webitproff@gmail.com>
2022-02-11 15:33:46 +0300

    Add files via upload

---
+++ attacherfreelance/attacherfreelance.setup.php
@@ -0,0 +1,22 @@
+<?php
+/* ====================
+[BEGIN_COT_EXT]
+Code=attacherfreelance
+Name=Attacherfreelance
+Category=files-media
+Description=Attaching images and files for freelance (projects,market,folio)
+Version=1.0.0.beta
+Date=2021-12-22
+Author=
+Copyright=Copyright (c) 2018 - 2022 
+Notes=BSD License
+Auth_guests=R1
+Lock_guests=2345A
+Auth_members=RW1
+Lock_members=2345
+Requires_plugins=attacher
+[END_COT_EXT]
+==================== */
+
+
+defined('COT_CODE') or die('Wrong URL.');
